ca36c792b31ce117d5580d33c547b13b3822a69b
ross creighton <rosson.creighton@gmail.com>
2017-03-26 15:48:18 -0400

    adds context manager helper for database transactions

--- data/database.py
+++ data/database.py
@@ -1,18 +1,30 @@
+from contextlib import contextmanager
+
 from sqlalchemy import engine
 from sqlalchemy.orm import sessionmaker
 
-from config import POSTGRES
-from models import Crumb
+from data.config import POSTGRES
+from data.models import Crumb
 
 postgres_url = str(engine.url.URL(**POSTGRES))
 engine = engine.create_engine(postgres_url, echo=True)
 Session = sessionmaker(bind=engine)
 
 
-def seed_database():
+@contextmanager
+def session_manager():
     session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
-    for num in range(0, 100):
-        session.add(Crumb())
 
-    session.commit()
+def seed_database():
+    with session_manager() as session:
+        for num in range(0, 100):
+            session.add(Crumb())
