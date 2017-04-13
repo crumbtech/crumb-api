dae446f0b094101a4ba88e823872ad87f7912e4f
ross creighton <rosson.creighton@gmail.com>
2017-04-12 20:05:01 -0400

    cleans up model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,62 +1,61 @@
 import bcrypt
 import pytest
 import sqlalchemy
+import faker
 
-import src.database as db
 import src.models as models
+import src.database as db
+import src.lib as lib
 
 
-class TestUser:
-    user_dict = dict(phone_number='+12345678910', password='test')
+fake = faker.Factory.create()
 
-    def delete_existing_users(self):
-        """ delete all records from users table
-        """
+
+def test_hash_password():
+    """ successfully hashes password
+    """
+    hashed = models.User.hash_password('password', bcrypt.gensalt())
+    assert type(hashed) == bytes
+
+
+def test_verify_password():
+    """ correctly tests passwords against the hashed password in the database
+    """
+    test_password = 'password'
+    user = models.User(phone_number='+12345678910', password=test_password)
+    with db.session_manager() as session:
+        session.add(user)
+        session.commit()
+        assert user.verify_password('invalid password') is False
+        assert user.verify_password(test_password) is True
+
+
+def test_create_user():
+    """ successfully creates a user record
+    """
+    test_phone = lib.normalize_phone_number('+1' + fake.phone_number())
+    user = models.User(phone_number=test_phone, password='password1')
+    with db.session_manager() as session:
+        session.add(user)
+        session.commit()
+        persisted_user = session.query(models.User).filter_by(
+            phone_number=test_phone).one()
+        assert isinstance(persisted_user.id, int) is True
+        token = persisted_user.generate_auth_token()
+        assert isinstance(token, str) is True
+        session.delete(persisted_user)
+
+
+def test_create_duplicate_user():
+    """ the database should not let us insert duplicate phone numbers
+    """
+    user1 = models.User(phone_number='+12345678910', password='password1')
+    user2 = models.User(phone_number='+12345678910', password='password2')
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
         with db.session_manager() as session:
-            for user in session.query(models.User):
-                session.delete(user)
-
-    def test_hash_password(self):
-        """ successfully hashes password
-        """
-        hashed = models.User.hash_password('password', bcrypt.gensalt())
-        assert type(hashed) == bytes
-
-    def test_verify_password(self):
-        """ correctly tests passwords against the hashed password in the database
-        """
-        user = models.User(**self.user_dict)
-        with db.session_manager() as session:
-            session.add(user)
+            session.add(user1)
             session.commit()
-            user.verify_password(self.user_dict['password'])
-            session.delete(user)
-
-    def test_create_user(self):
-        """ successfully creates a user record
-        """
-        user = models.User(**self.user_dict)
-        with db.session_manager() as session:
-            session.add(user)
+            session.add(user2)
             session.commit()
-            persisted_user = session.query(models.User).filter_by(
-                phone_number=self.user_dict['phone_number']).first()
-            assert isinstance(persisted_user.id, int) is True
-            token = persisted_user.generate_auth_token()
-            assert isinstance(token, str) is True
-            session.delete(persisted_user)
-
-    def test_create_duplicate_user(self):
-        """ the database should not let us insert duplicate phone numbers
-        """
-        self.delete_existing_users()
-        user1 = models.User(**self.user_dict)
-        user2 = models.User(**self.user_dict)
-        with pytest.raises(sqlalchemy.exc.IntegrityError):
-            with db.session_manager() as session:
-                session.add(user1)
-                session.commit()
-                session.add(user2)
-                session.commit()
-                session.delete(user1)
-                session.delete(user2)
+            session.delete(user1)
+            session.delete(user2)
